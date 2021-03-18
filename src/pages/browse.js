2c548025b9b4fbbb176e519604f3c3dc643e9abd
anhnamjr <anhnamnguyen99@gmail.com>
2021-03-18 18:17:27 +0700

    custom hook: useContent

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from "react";
+import { useContent } from "../hooks";
+import selectionFilter from "../utils/selection-filter"
 
 function Browse() {
-  return (
-    <div>
-      Hello from browse
-    </div>
-  );
+  const { series } = useContent("series");
+  const { films } = useContent("films");
+
+  const slides = selectionFilter({series, films})
+  console.log(slides)
+
+  return <div>Hello from browse</div>;
 }
 
 export default Browse;
